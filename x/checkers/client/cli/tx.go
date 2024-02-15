52213365d79019eb9ef86ed34c76278d722b98f4
TheFeenStudio <mbannakov@yahoo.com>
2024-02-15 14:09:38 +0300

    create post

--- x/checkers/client/cli/tx.go
+++ x/checkers/client/cli/tx.go
@@ -30,7 +30,9 @@ func GetTxCmd() *cobra.Command {
 		RunE:                       client.ValidateCmd,
 	}
 
-	// this line is used by starport scaffolding # 1
+	cmd.AddCommand(CmdCreatePost())
+cmd.AddCommand(CmdCreatePost())
+// this line is used by starport scaffolding # 1
 
 	return cmd
 }
